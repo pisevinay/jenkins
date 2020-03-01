93f5a37cbc9925fe6d8acee6b73625dc6c620712
pisevinay <47447451+pisevinay@users.noreply.github.com>
2020-03-01 13:27:30 +0530

    Delete sunday.java

--- sunday.java
+++
@@ -1,9 +0,0 @@
-class sunday{
-
-    public static void main(String args[])
-{
-     System.out.println("Hello welcome to New Year 2020");
-        System.out.println("Hello welcome to Zebra");
-    }
-}
-
